eecabd11c69c33ce75fd3bcd79cb231cfb04a56d
zentochi <102146659+zentochi@users.noreply.github.com>
2022-03-28 11:47:03 +0700

    Add files via upload

---
+++ LuasLingkaran.java
@@ -0,0 +1,25 @@
+package com.javax;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Danuarta
+ */
+public class LuasLingkaran {
+    public static void main(String[] args) { 
+    double phi = 3.14;
+    double luas,jarijari;
+    int confirm;
+    
+    JOptionPane.showMessageDialog(null,"Program menghitung Luas Lingkaran");
+    do{
+    jarijari = Double.parseDouble(JOptionPane.showInputDialog(null,"Masukkan Jari-jari"));
+    luas = jarijari*jarijari*phi;
+    JOptionPane.showMessageDialog(null, "Luas lingkaran adalah "+luas);
+    confirm = JOptionPane.showConfirmDialog(null, "Ingin mengulangi?","Konfirmasi",JOptionPane.YES_NO_OPTION);
+    }while(confirm == JOptionPane.YES_OPTION);
+    while(confirm == JOptionPane.NO_OPTION){
+    JOptionPane.showMessageDialog(null, "Program selesai !");
+    System.exit(0);
+    }
+  }
+}
