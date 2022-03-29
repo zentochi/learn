2f301c09df56d097221156099052dc854e760272
zentochi <102146659+zentochi@users.noreply.github.com>
2022-03-29 14:44:21 +0700

    Update LuasLingkaranSegitiga.java

--- LuasLingkaranSegitiga.java
+++ LuasLingkaranSegitiga.java
@@ -10,18 +10,19 @@ double phi = 3.14;
 double luaslingkaran,luassegitiga,jarijari,alas,tinggi;
 int confirm;
 String pilihan;
+  
 JOptionPane.showMessageDialog(null,"Program menghitung Luas Lingkaran dan Segitiga");
 do{ //blok perulangan jika dialog Ingin Mengulang ditekan YES
 pilihan = JOptionPane.showInputDialog(null,"Ketik Segitiiga atau Lingkaran");
 if(pilihan.equalsIgnoreCase("lingkaran")){
-jarijari = Double.parseDouble(JOptionPane.showInputDialog(null,"Masukkan Jari-jari"));
-luaslingkaran = jarijari*jarijari*phi;
-JOptionPane.showMessageDialog(null, "Luas lingkaran adalah "+luaslingkaran);
+  jarijari = Double.parseDouble(JOptionPane.showInputDialog(null,"Masukkan Jari-jari"));
+  luaslingkaran = jarijari*jarijari*phi;
+  JOptionPane.showMessageDialog(null, "Luas lingkaran adalah "+luaslingkaran);
 }else if(pilihan.equalsIgnoreCase("segitiga")){
-alas = Double.parseDouble(JOptionPane.showInputDialog(null,"Masukkan alas"));
-tinggi = Double.parseDouble(JOptionPane.showInputDialog(null,"Masukkan tinggi"));
-luassegitiga = alas*tinggi/2;
-JOptionPane.showMessageDialog(null, "Luas Segitiga adalah "+luassegitiga);
+  alas = Double.parseDouble(JOptionPane.showInputDialog(null,"Masukkan alas"));
+  tinggi = Double.parseDouble(JOptionPane.showInputDialog(null,"Masukkan tinggi"));
+  luassegitiga = alas*tinggi/2;
+  JOptionPane.showMessageDialog(null, "Luas Segitiga adalah "+luassegitiga);
 }
 confirm = JOptionPane.showConfirmDialog(null, "Ingin mengulangi?","Konfirmasi",
 JOptionPane.YES_NO_OPTION);//<-merubah pilihan tombol ke YES dan No
