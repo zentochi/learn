c8849881f6265a88e63ade54e0448184ea4993b2
zentochi <102146659+zentochi@users.noreply.github.com>
2022-04-03 20:04:25 +0700

    Add files via upload

---
+++ SegitigaBintang.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Danuarta
+ */
+public class SegitigaBintang {
+    public static void main(String[] args) {
+    int y,x,bintang=15;
+    for(y=1;y<=bintang;y++){
+        for(x=1;x<=y;x++){
+        System.out.print(x+" ");
+        }
+    System.out.println();
+    }     
+}  
+}
