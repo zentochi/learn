8d8b250bffb55e326fd74d2b7a9043860dbb423f
zentochi <102146659+zentochi@users.noreply.github.com>
2022-04-19 11:08:45 +0700

    Add files via upload

---
+++ Pegawai.java
@@ -0,0 +1,26 @@
+package com.belajar;
+/**
+ *
+ * @author PROG 33
+ */
+public class Pegawai extends Manusia{
+    String nip;
+    String bidangBagian;
+    String gaji;
+    
+    public void setGaji(String gaji){
+        this.gaji = gaji;
+    }
+    public String getGaji(){
+        return gaji;
+    }
+    public Pegawai(){
+    }
+    public Pegawai(String nip, String bidangBagian){
+        this.nip = nip;
+        this.bidangBagian = bidangBagian;
+    }
+    public void status(String Status){
+        System.out.println(Status);
+    }   
+}
---
+++ Siswa.java
@@ -0,0 +1,41 @@
+package com.belajar;
+/**
+ *
+ * @author Danuarta
+ */
+public class Siswa extends Manusia{
+    private String nis;
+    private String kelas;
+    private String waliKelas;
+
+    public Siswa(){}
+    
+    public Siswa(String nis, String kelas, String waliKelas){
+        this.nis = nis;
+        this.kelas = kelas;
+        this.waliKelas = waliKelas;
+    }
+    
+    public String getNis(){
+        return nis;
+    }
+    public void setNis(String nis){
+        this.nis = nis;
+    }
+    public String getKelas(){
+        return kelas;
+    }
+    public void setKelas(String kelas){
+        this.kelas = kelas;
+    }
+    public String getWalikelas(){
+    return waliKelas;
+    }
+    public void setWalikelas(String waliKelas){
+        this.waliKelas = waliKelas;
+    }
+    
+    public void status(){
+        System.out.println("Status : sedang belajar");
+    }  
+}
